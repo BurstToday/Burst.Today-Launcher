4a2cdb25ec021077ee5ba08ac3035492dcd16a44
VBdotNET <rileybryan@gmail.com>
2014-09-24 21:15:41 -0700

    Initial Commit of Launcher
    This downloads the miner, wallet, and Burst.Today application and starts
    Burst.Today

---
+++ Burst.Today Launcher/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar3 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.Location = New System.Drawing.Point(35, 34)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(250, 250)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.BackColor = System.Drawing.Color.White
+        Me.ProgressBar1.Location = New System.Drawing.Point(35, 322)
+        Me.ProgressBar1.Maximum = 110
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(250, 38)
+        Me.ProgressBar1.Step = 1
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.BackColor = System.Drawing.Color.White
+        Me.ProgressBar2.Location = New System.Drawing.Point(35, 394)
+        Me.ProgressBar2.Maximum = 110
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(250, 38)
+        Me.ProgressBar2.Step = 1
+        Me.ProgressBar2.TabIndex = 1
+        '
+        'ProgressBar3
+        '
+        Me.ProgressBar3.BackColor = System.Drawing.Color.White
+        Me.ProgressBar3.Location = New System.Drawing.Point(34, 467)
+        Me.ProgressBar3.Maximum = 110
+        Me.ProgressBar3.Name = "ProgressBar3"
+        Me.ProgressBar3.Size = New System.Drawing.Size(250, 38)
+        Me.ProgressBar3.Step = 1
+        Me.ProgressBar3.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(130, 295)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Wallet"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(133, 367)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Miner"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(110, 440)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Burst.Today"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(323, 536)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar3)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Burst.Today Launcher"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar3 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Burst.Today Launcher/Form1.vb
@@ -0,0 +1,201 @@
+﻿Public Class Form1
+
+    Dim WithEvents wc As System.Net.WebClient
+    Dim WithEvents wc2 As System.Net.WebClient
+    Dim WithEvents wc3 As System.Net.WebClient
+    Dim WalletDL As Integer = 0
+    Dim MinerDL As Integer = 0
+    Dim BurstTodayDL As Integer = 0
+
+    
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        Button1.PerformClick()
+    End Sub
+
+    'ProgressBar for Wallet
+    Private Sub DownloadProgress(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles wc.DownloadProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+        ProgressBar1.Refresh()
+    End Sub
+
+    'Progressbar for Miner
+    Private Sub DownloadProgress2(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles wc2.DownloadProgressChanged
+        ProgressBar2.Value = e.ProgressPercentage
+        ProgressBar2.Refresh()
+    End Sub
+
+    'Progressbar for Burst.Today
+    Private Sub DownloadProgress3(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles wc3.DownloadProgressChanged
+        ProgressBar3.Value = e.ProgressPercentage
+        ProgressBar3.Refresh()
+    End Sub
+
+    'Wallet Download Complete
+    Private Sub wc_DownloadFileCompleted(sender As Object, e As System.ComponentModel.AsyncCompletedEventArgs) Handles wc.DownloadFileCompleted
+        Try
+            If e.Cancelled Then
+
+            ElseIf e.Error IsNot Nothing Then
+                MessageBox.Show(e.Error.ToString(), "Download Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                'Extract the wallet
+                Using Zippo As Ionic.Zip.ZipFile = Ionic.Zip.ZipFile.Read("C:\Burst.Today\Wallet.zip")
+                    System.Threading.Thread.Sleep(100)
+                    Zippo.ExtractAll("C:\Burst.Today\Wallet\", Ionic.Zip.ExtractExistingFileAction.OverwriteSilently)
+                    ProgressBar1.Value = 110
+                    WalletDL = 1
+                    System.Threading.Thread.Sleep(500)
+                    ProgressBar1.ForeColor = Color.Green
+                    ProgressBar1.Refresh()
+                    If MinerDL = 1 And BurstTodayDL = 1 Then
+                        'MsgBox("1")
+                        StartBurstToday()
+                    End If
+                End Using
+            End If
+        Catch ex As Exception
+            ProgressBar1.ForeColor = Color.Red
+            ProgressBar1.Refresh()
+        End Try
+    End Sub
+
+    'Miner Download Complete
+    Private Sub wc2_DownloadFileCompleted(sender As Object, e As System.ComponentModel.AsyncCompletedEventArgs) Handles wc2.DownloadFileCompleted
+        Try
+            If e.Cancelled Then
+
+            ElseIf e.Error IsNot Nothing Then
+                MessageBox.Show(e.Error.ToString(), "Download Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                ' wc2.Dispose()
+                'Extract the Miner
+                Using Zippo As Ionic.Zip.ZipFile = Ionic.Zip.ZipFile.Read("C:\Burst.Today\Miner.zip")
+                    System.Threading.Thread.Sleep(100)
+                    Zippo.ExtractAll("C:\Burst.Today\Miner\", Ionic.Zip.ExtractExistingFileAction.OverwriteSilently)
+                    ProgressBar2.Value = 110
+                    MinerDL = 1
+                    System.Threading.Thread.Sleep(500)
+                    ProgressBar2.ForeColor = Color.DarkGreen
+                    ProgressBar2.Refresh()
+                    If WalletDL = 1 And BurstTodayDL = 1 Then
+                        'MsgBox("2")
+                        StartBurstToday()
+                    End If
+                End Using
+            End If
+        Catch ex As Exception
+            ProgressBar2.ForeColor = Color.Red
+            ProgressBar2.Refresh()
+
+
+        End Try
+    End Sub
+
+    'Burst.Today Download Complete
+    Private Sub wc3_DownloadFileCompleted(sender As Object, e As System.ComponentModel.AsyncCompletedEventArgs) Handles wc3.DownloadFileCompleted
+        Try
+            If e.Cancelled Then
+
+            ElseIf e.Error IsNot Nothing Then
+                MessageBox.Show(e.Error.ToString(), "Download Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                ' wc3.Dispose()
+                'Extract the Miner
+                Using Zippo As Ionic.Zip.ZipFile = Ionic.Zip.ZipFile.Read("C:\Burst.Today\Burst.Today.zip")
+                    System.Threading.Thread.Sleep(100)
+                    Zippo.ExtractAll("C:\Burst.Today\Burst.Today\", Ionic.Zip.ExtractExistingFileAction.OverwriteSilently)
+                    ProgressBar3.Value = 110
+                    BurstTodayDL = 1
+                    System.Threading.Thread.Sleep(500)
+                    ProgressBar3.ForeColor = Color.DarkGreen
+                    ProgressBar3.Refresh()
+                    If WalletDL = 1 And MinerDL = 1 Then
+                        'MsgBox("3")
+                        StartBurstToday()
+                    End If
+                End Using
+            End If
+        Catch ex As Exception
+            ProgressBar3.ForeColor = Color.Red
+            ProgressBar3.Refresh()
+        End Try
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Cursor = Cursors.WaitCursor
+
+        'Create Folders for download
+        'C:\Burst.Today
+        If My.Computer.FileSystem.DirectoryExists("C:\Burst.Today") Then
+            'Do Nothing, the folder Exists
+        Else
+            My.Computer.FileSystem.CreateDirectory("C:\Burst.Today")
+        End If
+
+        'C:\Burst.Today\Wallet\
+        If My.Computer.FileSystem.DirectoryExists("C:\Burst.Today\Wallet") Then
+            'Do Nothing, the folder Exists
+        Else
+            My.Computer.FileSystem.CreateDirectory("C:\Burst.Today\Wallet")
+        End If
+
+        'C:\Burst.Today\Miner\
+        If My.Computer.FileSystem.DirectoryExists("C:\Burst.Today\Miner") Then
+            'Do Nothing, the folder Exists
+        Else
+            My.Computer.FileSystem.CreateDirectory("C:\Burst.Today\Miner")
+        End If
+
+        'C:\Burst.Today\Burst.today\
+        If My.Computer.FileSystem.DirectoryExists("C:\Burst.Today\Burst.Today") Then
+            'Do Nothing, the folder Exists
+        Else
+            My.Computer.FileSystem.CreateDirectory("C:\Burst.Today\Burst.Today")
+        End If
+
+        'Wallet
+        Dim URL As String = "https://github.com/BurstProject/burstcoin/archive/master.zip"
+        Dim Path As String = "C:\Burst.Today\Wallet.zip"
+        wc = New System.Net.WebClient
+        wc.DownloadFileAsync(New Uri(URL), Path)
+        'Dim WalletTask As System.Threading.Tasks.Task = New Task(Of String)(Function()
+        'wc.DownloadFileAsync(New Uri(URL), Path)
+        'Return 1
+        'End Function)
+
+        'Miner
+        Dim URL2 As String = "https://github.com/BurstProject/pocminer/archive/master.zip"
+        Dim Path2 As String = "C:\Burst.Today\Miner.zip"
+        wc2 = New System.Net.WebClient
+        wc2.DownloadFileAsync(New Uri(URL2), Path2)
+        'Dim MinerTask As System.Threading.Tasks.Task = New Task(Of String)(Function()
+        'wc2.DownloadFileAsync(New Uri(URL2), Path2)
+        'Return 1
+        'End Function)
+
+        'Burst.Today
+        Dim URL3 As String = "https://github.com/BurstToday/BurstTodayUI/archive/master.zip"
+        Dim Path3 As String = "C:\Burst.Today\Burst.Today.zip"
+        wc3 = New System.Net.WebClient
+        wc3.DownloadFileAsync(New Uri(URL3), Path3)
+
+    End Sub
+
+    Private Sub StartBurstToday()
+        System.Threading.Thread.Sleep(1000)
+
+      
+        Dim compiler As New Process()
+        compiler.StartInfo.FileName = "C:\Burst.Today\Burst.Today\BurstTodayUI-master\Burst\bin\Debug\burst.exe"
+        compiler.StartInfo.UseShellExecute = False
+        compiler.Start()
+        Application.Exit()
+
+
+    End Sub
+
+
+End Class
